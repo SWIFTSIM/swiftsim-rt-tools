71cfe59e91ec762e3e7a6a21de33054a03b9792e
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-07-07 01:28:56 +0200

    initial commit iliev test 0

---
+++ additionalTests/Iliev0part3/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# make run.sh fail if a subcommand fails
+set -e
+set -o pipefail
+
+if [ ! -f ./ilievTest0part3.hdf5 ]; then
+    echo "creating ICs"
+    python3 makeIC.py
+fi
+
+# Run SWIFT with RT
+../../../swiftsim/examples/swift \
+    --hydro \
+    --threads=4 \
+    --verbose=0  \
+    --radiation \
+    --external-gravity \
+    --stars \
+    --feedback \
+./ilievTest0part3.yml 2>&1 | tee output.log
+    # --steps=10 \
+
+python3 plotSolution.py
