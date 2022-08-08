ec529908c11857189e47ca4b6289364834a844a3
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-08-08 09:46:15 +0200

    fixed iliev 0 part 2 setup.

--- additionalTests/Iliev0part2/plotSolution.py
+++ additionalTests/Iliev0part2/plotSolution.py
@@ -10,44 +10,42 @@ import stromgren_plotting_tools as spt
 import unyt
 from scipy import stats
 
-#  mpl.use('Gtk')
 
-
-# Plot parameters
-#  params = {
-#      "axes.labelsize": 14,
-#      "axes.titlesize": 14,
-#      "font.size": 14,
-#      "legend.fontsize": 14,
-#      "xtick.labelsize": 12,
-#      "ytick.labelsize": 12,
-#      "xtick.direction": "in",
-#      "ytick.direction": "in",
-#      "xtick.top": True,
-#      "ytick.right": True,
-#      "xtick.major.width": 1.5,
-#      "ytick.major.width": 1.5,
-#      "axes.linewidth": 1.5,
-#      "text.usetex": True,
-#      "figure.figsize": (5, 4),
-#      "figure.subplot.left": 0.045,
-#      "figure.subplot.right": 0.99,
-#      "figure.subplot.bottom": 0.05,
-#      "figure.subplot.top": 0.99,
-#      "figure.subplot.wspace": 0.15,
-#      "figure.subplot.hspace": 0.12,
-#      "lines.markersize": 1,
-#      "lines.linewidth": 2.0,
-#  }
-#  mpl.rcParams.update(params)
+#  Plot parameters
+params = {
+    "axes.labelsize": 14,
+    "axes.titlesize": 14,
+    "font.size": 14,
+    "legend.fontsize": 14,
+    "xtick.labelsize": 12,
+    "ytick.labelsize": 12,
+    "xtick.direction": "in",
+    "ytick.direction": "in",
+    "xtick.top": True,
+    "ytick.right": True,
+    "xtick.major.width": 1.5,
+    "ytick.major.width": 1.5,
+    "axes.linewidth": 1.5,
+    "text.usetex": True,
+    "figure.figsize": (5, 4),
+    "figure.subplot.left": 0.045,
+    "figure.subplot.right": 0.99,
+    "figure.subplot.bottom": 0.05,
+    "figure.subplot.top": 0.99,
+    "figure.subplot.wspace": 0.15,
+    "figure.subplot.hspace": 0.12,
+    "lines.markersize": 1,
+    "lines.linewidth": 2.0,
+}
+mpl.rcParams.update(params)
 
 
 scatterplot_kwargs = {
-    "alpha": 0.6,
-    "s": 4,
+    "alpha": 0.1,
+    "s": 2,
     "marker": ".",
     "linewidth": 0.0,
-    "facecolor": "blue",
+    #  "facecolor": "blue",
 }
 
 # Read in cmdline arg: Are we plotting only one snapshot, or all?
@@ -91,37 +89,40 @@ def plot_solution(filename):
     # get profiles
     # max r should be sqrt(3) * boxlen
     nbins = 100
-    r_bin_edges = np.linspace(0., 2., nbins + 1)
+    r_bin_edges = np.linspace(0., 1.4, nbins + 1)
     r_bin_centers = 0.5 * (r_bin_edges[:-1] + r_bin_edges[1:])
-    xHI_binned, _, _ = stats.binned_statistic(r, xHI, statistic="mean", bins=r_bin_edges, range=(0., 1.))
-    xHII_binned, _, _ = stats.binned_statistic(r, xHII, statistic="mean", bins=r_bin_edges, range=(0., 1.))
-    T_binned, _, _ = stats.binned_statistic(r, T, statistic="mean", bins=r_bin_edges, range=(0., 1.))
+    xHI_binned, _, _ = stats.binned_statistic(r, xHI, statistic="mean", bins=r_bin_edges, range=(0., 1.4))
+    xHII_binned, _, _ = stats.binned_statistic(r, xHII, statistic="mean", bins=r_bin_edges, range=(0., 1.4))
+    T_binned, _, _ = stats.binned_statistic(r, T, statistic="mean", bins=r_bin_edges, range=(0., 1.4))
 
     fig = plt.figure(figsize=(10, 5.5))
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
 
-    ax1.scatter(r, xHI, **scatterplot_kwargs)
-    ax1.scatter(r, xHII, **scatterplot_kwargs)
-    ax1.semilogy(r_bin_centers, xHI_binned, label=r"$x_{HI}$")
-    ax1.semilogy(r_bin_centers, xHII_binned, label=r"$x_{HII}$")
+    ax1.scatter(r, xHI, **scatterplot_kwargs, zorder = 0)
+    ax1.scatter(r, xHII, **scatterplot_kwargs, zorder = 0)
+    ax1.semilogy(r_bin_centers, xHI_binned, label=r"$x_{HI}$", zorder = 2)
+    ax1.semilogy(r_bin_centers, xHII_binned, label=r"$x_{HII}$", zorder = 2)
+    # black background lines
+    ax1.semilogy(r_bin_centers, xHI_binned, c="k", linewidth = params["lines.linewidth"]*2., zorder=1)
+    ax1.semilogy(r_bin_centers, xHII_binned, c="k", linewidth = params["lines.linewidth"]*2., zorder=1)
 
 
-    ax2.scatter(r, T, **scatterplot_kwargs)
-    ax2.semilogy(r_bin_centers, T_binned, label=r"gas temperature")
+    ax2.scatter(r, T, **scatterplot_kwargs, zorder = 0)
+    ax2.semilogy(r_bin_centers, T_binned, label=r"gas temperature", zorder = 2)
+    ax2.semilogy(r_bin_centers, T_binned, c="k", linewidth = params["lines.linewidth"]*2., zorder=1)
 
-
-    ax1.set_xlabel("r / L")
+    ax1.set_xlabel("r / (L / 2)")
     ax1.set_ylabel("Hydrogen Fractions")
-    ax1.set_xlim(0., 1.)
+    ax1.set_xlim(0., 1.4)
     ax1.legend()
 
-    ax2.set_xlabel("r / L")
+    ax2.set_xlabel("r / (L / 2)")
     ax2.set_ylabel("Temperature [K]")
-    ax2.set_xlim(0., 1.)
-    #  ax2.legend()
+    ax2.set_xlim(0., 1.4)
+    ax2.set_yscale("log")
 
-    #  plt.xlim([0, boxsize[0] / 2.0])
+    fig.suptitle("Iliev+06 Test 0 part 2, $t$ = {0:.0f}".format(meta.time.to("Myr")))
     plt.tight_layout()
     figname = filename[:-5]
     figname += ".png"
